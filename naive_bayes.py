c9245460d2d339bcfad59879dc86d8aa8b9f66df
eniseirem <eniseirem@gmail.com>
2020-11-25 03:28:28 +0300

    Initial commit

---
+++ naive_bayes.py
@@ -0,0 +1,20 @@
+#Bayes relies on conditional probability
+
+
+class NaiveBayes:
+    #TODO:maybe we do not need an init func
+
+    def fit(self): #this will be fitting the training
+                    #TODO:Fit can be also be part of preprocess ??
+        #TODO: need mean for each class (the function need the datasets for it)
+                    #todo: varience of each class
+
+        pass
+
+    def train(self): #usage of train
+                    #magic happens ?
+        pass
+
+    def predict(self):
+                    #last one
+        pass
