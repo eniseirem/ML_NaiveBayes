e45b8e370d3965a91be494f993ac6f73426b8e45
eniseirem <eniseirem@gmail.com>
2020-11-29 05:14:11 +0300

    playing around with split funct

--- preprocess.py
+++ preprocess.py
@@ -26,3 +26,12 @@ data = pd.read_csv('data/abalone.data', names=names,na_values=missing_values)
 #print(data.tail)
 print(data.shape)
 
+#I'll split the data here since both of the questions needs same type of it.
+
+from sklearn.model_selection import train_test_split
+
+data = data.sample(frac=1, random_state=42) #mixing database for more random sample
+
+def tt(X,y, sample):
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=sample,  random_state=1) #for calling from
+    return [X_train, X_valid, y_train, y_valid] #TODO: Add Keywords
