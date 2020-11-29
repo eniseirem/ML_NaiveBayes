976af06ffb600e035bfef8b9322521322a8d5f64
eniseirem <eniseirem@gmail.com>
2020-11-29 04:44:19 +0300

    added names

--- preprocess.py
+++ preprocess.py
@@ -5,9 +5,24 @@ import numpy as np
 
 #I'm adding these too avoid any type of na value.
 missing_values = ["n/a", "na", "--", " ?","?"]
-names=[] #todo: add names of variables
+
+    # Name		Data Type	Meas.	Description
+	# ----		---------	-----	-----------
+	# Sex		nominal			M, F, and I (infant)
+	# Length		continuous	mm	Longest shell measurement
+	# Diameter	continuous	mm	perpendicular to length
+	# Height		continuous	mm	with meat in shell
+	# Whole weight	continuous	grams	whole abalone
+	# Shucked weight	continuous	grams	weight of meat
+	# Viscera weight	continuous	grams	gut weight (after bleeding)
+	# Shell weight	continuous	grams	after being dried
+	# Rings		integer			+1.5 gives the age in years
+
+names=["Sex", "Length", "Diameter", "Height", "Whole-w", "Shucked-w","Viscera-w", "Shell-w", "Rings"]
 #reading the data
-data = pd.read_csv('data/abalone.data', na_values=missing_values)
+data = pd.read_csv('data/abalone.data', names=names,na_values=missing_values)
 
 #checking the dataset
-print(data.tail)
+#print(data.tail)
+print(data.shape)
+
